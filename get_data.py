d15a1b3e268a0767f12a899e951fc22e7d556b66
Luc Gombert <luc.gombert@etu.univ-lyon1.fr>
2020-01-05 21:53:06 +0100

    amélioration de la récupération des données, sauvegarde en chemin via pickle, et mise en forme des données

--- get_data.py
+++ get_data.py
@@ -1,91 +1,183 @@
-import urllib.request
 import json
-from random import randrange
 import requests
-import os
-from github import Github
-import auth
+from sys import stdout
+from time import sleep
+import pickle
 
 base_url = "https://api.github.com/"
 filename = "data4.json"
 # To authenticate, copy "auth.json.skeleton" as "auth.json" (included in .gitignore) and fill it in
-#client_id, oauth_token = auth.get_auth_data("auth.json")
+# client_id, oauth_token = auth.get_auth_data("auth.json")
 USER_TOKEN = "NellyBARRET"
-ACCESS_TOKEN = '4bd9e8825dfcbad7a55d377d76b8d15eb9df7139'
+ACCESS_TOKEN = '44e7f84e9ac3898686786c872611e701ac9aa04b'
 
 
-def get_languages(owner, project_name):
-    complete_url = f"{base_url}repos/{owner}/{project_name}/languages"
-    contents = urllib.request.urlopen(complete_url).read()
-    return json.loads(contents)
-
-
-def get_year(owner, project_name):
+def get_repo(repo_name, repo_user):
+    """Get infos about a specific repo
+    @type repo_name: str
+    @param repo_name: the name of the user
+    @type repo_user: str
+    @param repo_user: the name of the repo
     """
-    TODO : récupération de l'année du projet
-    """
-    if randrange(1, 1000) < 500:
-        return 2010
-    else:
-        if randrange(1, 1000) < 500:
-            return 2011
-        else:
-            return 2012
-
-
-def getRepositoriesSince(since):
-    # print("getting repo since " + str(since))
-    complete_url = f"{base_url}repositories?since={since}"
+    complete_url = base_url + f"repos/{repo_user}/{repo_name}"
     contents = requests.get(complete_url, auth=(USER_TOKEN, ACCESS_TOKEN))
-    return contents.json()
-
-
-def getGoodInfos(repo):
-    repo = {'year': get_year(repo[0]['owner']['login'], repo[0]['name']),
-              'languages': get_languages(repo[0]['owner']['login'], repo[0]['name'])}
-    return repo
-
-
-def getAllRepositories():
-    local_repo = getRepositoriesSince(1)
-    all_repositories = []
-    all_repositories.append(getGoodInfos(local_repo))
-    if type(local_repo) is list:
-        repo_id = local_repo[len(local_repo) - 1]["id"]
+    repo = contents.json()
+    if "message" in repo:
+        if "Bad credentials" in repo["message"]:
+            raise Exception("Problème d'authentification")
+        elif "Not Found" in repo["message"]:
+            raise Exception("Repo inconnu")
     else:
-        repo_id = local_repo["id"]
-    while (local_repo and repo_id < 1000):
-        local_repo = getRepositoriesSince(repo_id)
-        repo_id = local_repo[len(local_repo) - 1]["id"]
-        all_repositories.append(getGoodInfos(local_repo))
-    return all_repositories
+        return repo
+
+
+def get_repos_since(since=1):
+    """Get repositoties since an id
+    @type since: int
+    @param since: the id where the scrapping begins
+    @rtype: int and json
+    @returns: the last id of the scrapped repo and the json content of scrapped repos
+    """
+    complete_url = base_url + "repositories?since=" + str(since)
+    contents = requests.get(complete_url, auth=(USER_TOKEN, ACCESS_TOKEN))
+    while "message" in contents.json() and "rate limit" in contents.json()["message"]:
+        # waiting for an hour
+        for i in range(0, 3601):
+            stdout.write("\r%d" % i)
+            stdout.flush()
+            sleep(1)
+        stdout.write("\n")
+        contents = requests.get(complete_url, auth=(USER_TOKEN, ACCESS_TOKEN))
+    return contents.json()[-1]["id"], contents.json()  # dernier id du since + les repos
+
+
+def shape_data(repo):
+    """Selects relevant fields in the repo
+    @type repo: json
+    @param repo: The current json repo
+    @rtype: json
+    @returns: a dict containing relevant fields of the repo
+    """
+    current_repo = {}
+    fields = ["language", "year", "stargazers_url", "forks_count"]
+    for field in fields:
+        if field == "year":
+            # special handling for year field
+            try:
+                current_repo["year"] = repo["created_at"][:4]
+            except:
+                pass
+        elif "_url" in field:
+            # url => request
+            try:
+                content_field = repo[field]
+                url_field = content_field
+                response = requests.get(url_field, auth=(USER_TOKEN, ACCESS_TOKEN))
+                field_name = field[:-4]
+                if field == "stargazers_url":
+                    # special handling for stars field (need to go through each user)
+                    current_repo[field_name] = len(response.json())
+                else:
+                    current_repo[field_name] = response.json()
+            except:
+                pass
+        else:
+            # no url => get the field without processing
+            try:
+                current_repo[field] = repo[field]
+            except:
+                pass
+    return current_repo
+
+
+def complete_sahped_datas(until):
+    """ Get full repositories data, truncated by shape_data, since an id
+    :type until: int
+    :param until: the id where the scrapping will ends
+    :return: The json content of scrapped repos
+    """
+    actual = 0
+    complete = []
+    while actual < until:
+        actual, contents = get_repos_since(actual)
+        for repo in contents:
+            name = repo["name"]
+            user = repo["owner"]["login"]
+            complete.append(shape_data(get_repo(name, user)))
+    return complete
 
 
 def get_in_shape(data):
-    """
-      Transforme les données brutes récupérées par getAllRepositories en données utilisables simplement pour le chord diagramme et le rayon de soleil :
-       - Un fichier de sortie contenant l'utilisation des langagues au format JSON pour les rayons de soleil : { "Ruby": 34, "C": 50 ...}
-       - Un fichier contenant la force des liens entre languages : {('Ruby', 'C'): 26, ('php', 'Ruby'): 55, ...}
-    """
+    """ Transforme les données brutes récupérée en données utilisables simplement pour nos diagrammes :
+     - Un premier fichier, contenant l'utilisation des langagues, indexé selon les années
+     - Un second fichier, contenant une matrice du nombre de projets en commun entre les langages, indexé selon les années => un index des langages et une matrice par année
+     - Un second fichier contenant les métriques par langage (nb_projets, stars, forks) """
 
-    # Fichier contenant l'utilisation, répartie par année
+    # Pour l'utilisation
     comptage = {}
-    for d in data:  # Parcours de chacun des repositories
-        languages = d['languages']
-        annee = d['year']
-        if not(annee in comptage):
-            comptage[annee] = {}
-        sum_repo = 0
-        for l in languages.keys():
-            sum_repo += languages.get(l, 0)
-        for l in languages.keys():  # Parcours de chacun des langages utilisés
-            comptage[annee][l] = comptage[annee].get(l, 0) + 1 * (languages.get(l, 0) / sum_repo)
+
+    # Pour la matrice
+    abscisse = {}
+    matrice = {}
+    cpt = 0
+
+    # Pour les métriques
+    metrics = {}
+
+    # Premier parcours pour trouver l'ensemble des langages existants. C'est un double parcours pour simplifier le code de la matrice après
+    for d in data:
+        if 'language' in d:
+            if not d['language'] in abscisse:
+                abscisse[d['language']] = cpt
+
+    # Puis calcul de l'ensemble des données
+    for d in data:
+
+        # Calcul de l'utilisation de chacun des langages
+        if 'language' in d and 'year' in d:
+            languages = d['language']
+            annee = d['year']
+            if not (annee in comptage):
+                comptage[annee] = {}
+            sum_repo = 0
+            for l in languages.keys():
+                sum_repo += languages.get(l, 0)
+            for l in languages.keys():  # Parcours de chacun des langages utilisés
+                comptage[annee][l] = comptage[annee].get(l, 0) + 1 * (languages.get(l, 0) / sum_repo)
+
+        # Calcul de la matrice du nombre de projets en commun pour chaque langage
+        if 'language' in d and 'year' in d:
+            languages = d['language']
+            annee = d['year']
+            if not (annee in matrice):
+                matrice[annee] = [[0] * len(abscisse)] * len(abscisse)
+            for l1 in languages.keys():
+                for l2 in languages.keys():
+                    matrice[abscisse[l1], abscisse[l2]] += 1
+
+        # Calcul des métriques pour chaque language
+        for l in abscisse.keys():
+            metrics[l] = {"nb_projects": 0, "stars": 0, "forks": 0}
+        if 'language' in d:
+            metrics[d['langage']] = {"nb_projects": metrics[d['langage']]['nb_projects'] + 1,
+                                     "stars": metrics[d['langage']]['stars'] + d['stargazers'],
+                                     "forks": metrics[d['langage']]['forks'] + d['forks_count']}
+
+    # Et enfin, export des données dans chacun des fichiers
     f = open("usage.json", "w")
     f.write(json.dumps(comptage))
+    f.close()
+    f = open("matrice.json", "w")
+    f.write(json.dumps(abscisse) + ',' + json.dumps(matrice))
+    f.close()
+    f = open("metrics.json", "w")
+    f.write(json.dumps(metrics))
     f.close()
 
 
 if __name__ == "__main__":
-    all_r = getAllRepositories()
-    print(all_r)
-    get_in_shape(all_r)
+    data = complete_sahped_datas(700)
+    pickle.dump(data, open('pickle', 'wb'))
+
+    data = pickle.load(open('pickle', 'rb'))
+    get_in_shape(data)
